bbd24c9fdd930c063d390a6219adb94e87fa586f
Renan Martins <renanrodm@gmail.com>
2026-02-20 14:07:53 -0300

    Adiciona curso_schema

---
+++ schemas/curso_schema.py
@@ -0,0 +1,18 @@
+"""
+    Para cada model teremos um schema.
+"""
+
+
+from typing import Optional
+from pydantic import BaseModel as SCBaseModel
+
+
+
+class CursoSchema(SCBaseModel):
+    id: Optional[int] #Opcional porque é criado pelo proprio banco de dados.
+    titulo: str
+    aulas: int
+    horas: int
+
+    class Config:
+        orm_mode = True
