7fa445fded2e57fc498e540106f7dee8b49372ad
Renan Martins <renanrodm@gmail.com>
2026-02-23 15:18:39 -0300

    Adiciona tabela (com db criado via postgresCLI); Adiciona rotas dentro do contexto específico de API

--- core/configs.py
+++ core/configs.py
@@ -1,7 +1,7 @@
 #Configurações gerais acessiveis a qualquer parte do nosso projeto
 
 
-from typing import List
+from typing import ClassVar, List
 
 from pydantic_settings import BaseSettings
 from pydantic import AnyHttpUrl
@@ -13,8 +13,8 @@ class Settings(BaseSettings):
     Configurações gerais usadas na aplicação
     """
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg://username:password@localhost:5432/faculdade'
-    DBBaseModel = declarative_base()
+    DB_URL: str = 'postgresql+asyncpg://postgres:password@localhost:5432/faculdade'
+    DBBaseModel: ClassVar = declarative_base()
 
     class Config:
         case_sensitive = True
