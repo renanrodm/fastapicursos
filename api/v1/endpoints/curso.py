6fdf17ffe621acc6c223abbf3b6bae827b6a9775
Renan Martins <renanrodm@gmail.com>
2026-02-23 15:33:21 -0300

    Adiciona rota POST /{curso_id} para deletar curso caso ele exista.

--- api/v1/endpoints/curso.py
+++ api/v1/endpoints/curso.py
@@ -65,3 +65,19 @@ async def put_curso(curso_id: int, curso: CursoSchema, db: AsyncSession = Depend
             return curso_up
         else:
             raise HTTPException(detail='Curso não encontrado.', status_code=status.HTTP_404_NOT_FOUND)
+        
+#DELETE curso
+@router.delete('/{curso_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_curso(curso_id: int, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso_del  = result.scalar_one_or_none()
+
+        if curso_del:
+            await session.delete(curso_del)
+            await session.commit()
+
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else:
+            raise HTTPException(detail='Curso não encontrado.', status_code=status.HTTP_404_NOT_FOUND)
